1c3f47a296a40dbc7f2fcb75aea9d689360ef29d
Damian Mann <damianmann12@gmail.com>
2022-07-15 13:20:02 +1000

    Section 5.1 Complete

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,20 +1,18 @@
 <x-layout>
-    @foreach ($posts as $post)
-        <article>
-            <h1>
-                <a href="/posts/{{ $post->slug }}">
-                    {!! $post->title !!}
-                </a>
-            </h1>
+    @include ('_posts-header')
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        <x-post-featured-card />
 
-            <p>
-                By <a href="/author/{{ $post->author->username }}">{{ $post->author->name }}</a> in:
-                <a class ="tag" href="/categories/{{ $post->category->slug }}">
-                   {{ $post->category->name }}
-                </a>
-            </p>
-            <h3>{{ $post->date }}</h3>
-            <p>{{ $post->excerpt }}</p>
-        </article>
-    @endforeach
+        <div class="lg:grid lg:grid-cols-2">
+            <x-post-card />
+            <x-post-card />
+        </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+            <x-post-card />
+            <x-post-card />
+            <x-post-card />
+        </div>
+
+    </main>
 </x-layout>
